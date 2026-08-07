ed1f21e3b41218b28219c581488280f73680e02e
Keshav Raj Sharma <jdkeshav01@gmail.com>
2026-08-07 21:54:59 +0545

    add: theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+import { ThemeProvider } from "@/components/providers/theme-provider";
 import { siteConfig } from "@/data/site";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -67,9 +70,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   );
